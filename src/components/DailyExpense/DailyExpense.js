6e84c5d3f9e30a13a87cdaf8a128aabeaddd6629
Monali C G <cgmonali@gmail.com>
2023-11-04 15:30:03 +0530

    Integrating it with firebase

--- src/components/DailyExpense/DailyExpense.js
+++ src/components/DailyExpense/DailyExpense.js
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 import classes from './DailyExpense.module.css';
 
 const DailyExpense = () => {
@@ -6,15 +6,38 @@ const DailyExpense = () => {
   const descriptionRef = useRef();
   const categoryRef = useRef();
 
-  const [expenses, setExpenses] = useState([]); 
+  const [expenses, setExpenses] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
-  function dailyExpenseHandler(event) {
+  useEffect(() => {
+    // Fetch expenses from the backend and update the expenses state
+    fetchExpenses();
+  }, []); // Empty dependency array ensures this runs only once on component load
+
+  async function fetchExpenses() {
+    setIsLoading(true);
+    try {
+      const response = await fetch('https://expense-tracker-item-default-rtdb.firebaseio.com/addexpenses.json'); // Replace with your backend API URL
+      if (response.ok) {
+        const data = await response.json();
+        const expensesArray = Object.keys(data).map((expenseId) => ({
+            id: expenseId,
+            ...data[expenseId],
+          }));
+        console.log(expensesArray)
+        setExpenses(expensesArray)
+      }
+    } catch (error) {
+      console.error('Error fetching expenses:', error);
+    }
+    setIsLoading(false);
+  }
+
+  async function dailyExpenseHandler(event) {
     event.preventDefault();
-    
     const money = moneyRef.current.value;
     const description = descriptionRef.current.value;
     const category = categoryRef.current.value;
-
 
     const newExpense = {
       money,
@@ -22,13 +45,28 @@ const DailyExpense = () => {
       category,
     };
 
-   
-    setExpenses((prevExpenses) => [...prevExpenses, newExpense]);
+    // Send the new expense to the backend and update expenses on success
+    const response = await saveExpenseToBackend(newExpense);
 
-  
+    if (response.ok) {
+      // Fetch the updated expenses after adding a new one
+      fetchExpenses();
+    }
+
+    // Clear the input fields
     moneyRef.current.value = '';
     descriptionRef.current.value = '';
-    categoryRef.current.value = ''; 
+    categoryRef.current.value = '';
+  }
+
+  async function saveExpenseToBackend(expense) {
+    return await fetch('https://expense-tracker-item-default-rtdb.firebaseio.com/addexpenses.json', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(expense),
+    });
   }
 
   return (
@@ -46,7 +84,7 @@ const DailyExpense = () => {
           </div>
           <div  >
             <label htmlFor="category">Category</label>
-            <select name="category" id="category" ref={categoryRef} value=""className={classes.selectInput}>
+            <select name="category" id="category" ref={categoryRef} className={classes.selectInput}>
   <option value="">Select Category</option>
   <option value="Food">Food</option>
   <option value="Petrol">Petrol</option>
@@ -57,7 +95,9 @@ const DailyExpense = () => {
         </form>
       </section>
 
-      {expenses.length > 0 && (
+      {isLoading ? (
+        <h2 style={{textAlign:'center'}}>Loading expenses...</h2>
+      ) : (
         <section className={classes.listingcontainer}>
           <h2>Expenses</h2>
           <ul>
